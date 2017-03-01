d25a2d94e2d8d4477be49b47f92c8d32d8a65c44
steve <vnsgbt@me.com>
2017-03-01 06:21:31 -0600

    add jest and enzyme tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App/>, div);
 });
+
+it('renders an App-body', () => {
+  const wrapper = shallow(<App />);
+  const footer = <div className="footer" id="footer">Snapshot</div>;
+  expect(wrapper.contains(footer)).toEqual(true);
+});
+
